72f86750fa05b63b38632410a0881c73783b5b25
leo-note <karen.koizumi7@gmail.com>
2021-07-30 17:16:47 +0900

    開発着手時のツール導入等
    ・Rubocopの導入
    ・turbolinksの無効化
    ・不要な変更履歴の除外

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start() // JavaScriptを正常に動作させるため無効化
 require("@rails/activestorage").start()
 require("channels")
 
